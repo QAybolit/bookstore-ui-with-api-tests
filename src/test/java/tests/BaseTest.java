6b84ea7cef0a0bb977379e3a2f2e3dd004707690
Dina <sd@myexe.net>
2025-11-12 17:22:08 +0300

    Delete config manager

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,28 +1,46 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import config.ConfigManager;
 import config.ProjectConfig;
-import config.ProjectConfigReader;
 import dto.LoginResponse;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import io.restassured.RestAssured;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class BaseTest {
 
-    private static final ProjectConfig projectConfig = ProjectConfigReader.INSTANCE.projectConfig();
+    private static final ProjectConfig projectConfig = ConfigFactory.create(ProjectConfig.class, System.getProperties());
     public LoginResponse loginResponse;
 
     @BeforeAll
     public static void setUp() {
-        ConfigManager configManager = new ConfigManager(projectConfig);
-        configManager.setApiConfig();
-        configManager.setWebConfig();
+        RestAssured.baseURI = projectConfig.baseUrl();
+        Configuration.baseUrl = projectConfig.baseUrl();
+        Configuration.pageLoadStrategy = projectConfig.pageLoadStrategy();
+        Configuration.timeout = projectConfig.timeout();
+        Configuration.browser = projectConfig.browser();
+        Configuration.browserVersion = projectConfig.browserVersion();
+        Configuration.browserSize = projectConfig.browserSize();
+
+        if (projectConfig.isRemote()) {
+            Configuration.remote = projectConfig.remoteUrl();
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", projectConfig.enableVnc(),
+                    "enableVideo", projectConfig.enableVideo()
+            ));
+            Configuration.browserCapabilities = capabilities;
+        }
     }
 
     @BeforeEach
